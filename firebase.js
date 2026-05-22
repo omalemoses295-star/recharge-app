f89bb7dbe9ebb536949ea917c48d32acb5b0990a
omalemoses295-star <omalemoses295@gmail.com>
2026-05-22 08:19:48 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDI_Lwtd8k3mj6CT5gkV6-gS6YOJ3IEiwI",
+  authDomain: "recharge-app-db556.firebaseapp.com",
+  projectId: "recharge-app-db556",
+  storageBucket: "recharge-app-db556.firebasestorage.app",
+  messagingSenderId: "558444254437",
+  appId: "1:558444254437:web:9632908bb32fc85697f00c",
+  measurementId: "G-FBWT3WLP9Q"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
